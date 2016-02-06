7ab8abb544ac464663fd332cd7fb117827562863
Alex <git@alexxx.co.uk>
2016-02-06 12:11:09 +0000

    Ship position, server screen

--- sockets.js
+++ sockets.js
@@ -15,7 +15,7 @@ exports.init = (server) => {
             socket.room = roomID;
             socket.role = 'host';
             games[roomID] = state.init();
-            //attachHandlers(games[roomID]);
+            attachHandlers(games[roomID]);
             socket.emit('game:created', roomID);
             console.log('game:create ' + roomID);
         });
