754408f30bbb498bd6b9489f9e2ab78b72914493
Alex <git@alexxx.co.uk>
2016-02-07 11:25:19 +0000

    Health types, power handling, UI tweaks

--- index.js
+++ index.js
@@ -3,14 +3,5 @@
 let express = require('express');
 var app = express();
 var server = app.listen(3000);
-
-// app.use(express.static('debug'));
 app.use(express.static('public'));
-
-/*app.get('/', function (req, res) {
-  res.send('Hello World!');
-  //Dan's shit here
-});*/
-
-
 require('./sockets.js').init(server);
